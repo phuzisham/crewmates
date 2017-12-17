698905dc888d1778fce8e584112c87ebdc803a84
David Hale <david.hale7@gmail.com>
2017-12-17 12:36:03 -0800

    displaying users on index

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+
+#seeding products
+20.times do
+  User.create!(name: Faker::Simpsons.unique.character,
+              age: Faker::Number.between(18, 80),
+              experience: Faker::Number.between(5, 20),
+              home: Faker::ElderScrolls.region,
+              username: Faker::Zelda.unique.character,
+              email: Faker::Internet.unique.email,
+              password: Faker::Internet.password(6),
+              avatar: URI.parse(Faker::Avatar.image))
+end
+
+p "Created #{User.count} users"
