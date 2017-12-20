2c74d98e688f2ecb45cfa2b3756638ef888df4a0
David Hale <david.hale7@gmail.com>
2017-12-20 14:14:21 -0800

    add detail to attributes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ class ApplicationController < ActionController::Base
   protected
 
     def configure_permitted_parameters
-      added_attrs = [:username, :email, :name, :home, :age, :experience, :avatar, :available, :account_type, :password, :password_confirmation, :remember_me]
-      update_attrs = [:username, :email, :name, :home, :age, :experience, :avatar, :available, :account_type, :password, :password_confirmation, :current_password]
+      added_attrs = [:username, :email, :name, :home, :age, :experience, :avatar, :available, :account_type, :detail, :password, :password_confirmation, :remember_me]
+      update_attrs = [:username, :email, :name, :home, :age, :experience, :avatar, :available, :account_type, :detail, :password, :password_confirmation, :current_password]
       devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
       devise_parameter_sanitizer.permit :account_update, keys: update_attrs
     end
