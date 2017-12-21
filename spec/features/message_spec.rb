84a8648f6f0175bbd5e35f0816d2eca9f51e6256
David Hale <david.hale7@gmail.com>
2017-12-21 15:02:38 -0800

    create message spec

---
+++ spec/features/message_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe 'test for messages' do
+  before(:each) do
+    # User.destroy_all
+    FactoryBot.create(:user)
+    user = User.create!(:email => 'huh@tEsT.com', :username => 'testUser', :password => 'password', :name => 'wILey MaGOo', :home => 'iStANbul' )
+    visit new_user_session_url
+    fill_in 'Username', :with => 'test'
+    fill_in 'Password', :with => 'password'
+    click_button 'Log in'
+  end
+
+  it 'will let user sign in' do
+    click_link 'Mailbox (0)'
+    click_link 'Message me!'
+    fill_in 'message_content', :with => 'This is a message.'
+    click_button 'Create Message'
+    expect(page).to(have_content('This is a message.'))
+  end
+
+end
