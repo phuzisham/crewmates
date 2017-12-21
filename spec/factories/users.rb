9f08b50e325c01514f6e1b8c14768183d84e3c8b
David Hale <david.hale7@gmail.com>
2017-12-20 16:01:06 -0800

    specs

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,8 @@
 FactoryBot.define do
 
   factory :user do
+    home('Montana')
+    name('Wiley Magoo')
     username('test')
     email('test@test.com')
     password('password')
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
+
+
+RSpec.describe User do
   before(:each) do
     FactoryBot.create(:user)
   end
-
   it { should validate_presence_of :email }
   it { should validate_presence_of :username }
   it { should validate_presence_of :password }
