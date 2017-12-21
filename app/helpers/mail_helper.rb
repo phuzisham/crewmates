1cf9a81e23eb3ac2b8a7edf85b854e71c6f4e709
David Hale <david.hale7@gmail.com>
2017-12-21 10:01:31 -0800

    add mail_count helper to display unread message count from individual users

--- app/helpers/mail_helper.rb
+++ app/helpers/mail_helper.rb
@@ -11,4 +11,17 @@ module MailHelper
     end
     return userMessages.count
   end
+
+  def mail_count(user1, user2)
+    userConv = Conversation.userMessages(user1.id)
+    userMessages = [];
+    userConv.each do |c|
+      c.messages.unread.each do |m|
+        if m.user_id != user1.id && m.user_id == user2.id
+          userMessages = userMessages.push(m)
+        end
+      end
+    end
+    return userMessages.count
+  end
 end
