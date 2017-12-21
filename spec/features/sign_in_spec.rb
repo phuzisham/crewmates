f823948e191e9264198ab88abe429695392af72b
David Hale <david.hale7@gmail.com>
2017-12-20 19:53:52 -0800

    sign in spec passing

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -4,7 +4,7 @@ describe 'test for sign-in process' do
   it 'will let user sign in' do
     FactoryBot.create(:user)
     visit new_user_session_url
-    fill_in 'Username', :with => 'user'
+    fill_in 'Username', :with => 'test'
     fill_in 'Password', :with => 'password'
     click_button 'Log in'
     expect(page).to have_content "Signed in successfully."
