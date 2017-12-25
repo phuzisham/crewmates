eea3c245135325b2550eab95ed830c0aa2be0b7b
David Hale <david.hale7@gmail.com>
2017-12-25 10:07:58 -0800

    29 specs passing

--- spec/features/message_spec.rb
+++ spec/features/message_spec.rb
@@ -11,12 +11,25 @@ describe 'test for messages' do
     click_button 'Log in'
   end
 
-  it 'will let user sign in' do
+  it 'will let user create message' do
     click_link 'Mailbox (0)'
     click_link 'Message me!'
     fill_in 'message_content', :with => 'This is a message.'
     click_button 'Create Message'
     expect(page).to(have_content('This is a message.'))
+  end
+
+  it 'will show user number of unread messages' do
+    click_link 'Mailbox (0)'
+    click_link 'Message me!'
+    fill_in 'message_content', :with => 'This is a message.'
+    click_button 'Create Message'
+    click_link 'Logout'
+    visit new_user_session_url
+    fill_in 'Username', :with => 'testUser'
+    fill_in 'Password', :with => 'password'
+    click_button 'Log in'
+    expect(page).to(have_content('Mailbox (1)'))
   end
 
 end
