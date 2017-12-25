5142b32cdfa117e75169893baa5bfe55a7c45b26
David Hale <david.hale7@gmail.com>
2017-12-25 10:49:13 -0800

    seeding forums and forum_messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,6 @@
 User.destroy_all
+Forum.destroy_all
+ForumMessage.destroy_all
 
 #seeding products
 20.times do
@@ -29,4 +31,25 @@ end
               avatar: URI.parse(Faker::Avatar.image))
 end
 
-p "Created #{User.count} members!"
+30.times do |i|
+  user = User.find(rand(User.first.id..User.last.id))
+  Forum.create!(title: Faker::Book.title,
+                description: Faker::Hipster.sentence,
+                user_id: user.id
+  )
+end
+
+100.times do |i|
+  board = Forum.find(rand(Forum.first.id..Forum.last.id))
+  user = User.find(rand(User.first.id..User.last.id))
+  ForumMessage.create!(user_id: user.id,
+                       forum_id: board.id,
+                       title: Faker::Simpsons.location,
+                       body: Faker::RickAndMorty.quote
+  )
+end
+
+
+p "Created #{User.count} users!"
+p "Created #{Forum.count} forums!"
+p "Created #{ForumMessage.count} forum messages!"
